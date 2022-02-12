27e249948beb6f2ff20e1f531a83b663b0291cfc
Douglasdejesus1 <douglas.dj1242@gmail.com>
2022-02-12 07:22:33 -0300

    prject chess, testeFile and teste ExceptionTryCatch

--- Testelogica/src/application/Program.java
+++ Testelogica/src/application/Program.java
@@ -5,14 +5,6 @@ import java.util.Date;
 
 public class Program {
 
-		public String solucao(String horario, int minutosAcrescimo) {
-			
-			SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-			
-			Date x= new Date(String.valueOf(horario) + minutosAcrescimo);
-			return sdf.format(x);
-		}
 		
-		solucao(3:00;30);
 }
 
