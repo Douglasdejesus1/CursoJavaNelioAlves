01513dda373f29ee1a44f9c7e67dbb94de0b19be
Douglasdejesus1 <douglas.dj1242@gmail.com>
2022-02-13 06:40:18 -0300

    teste com Writer and BufferedWriter

---
+++ TesteFileWhiterBufferedWriter/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String[] lines = new String[] {"Good Morning", "Googe afternoon", "good nigth"};
+		
+		String path = "C:\\Users\\Douglas\\Desktop\\new paste\\JavaNewTexto.txt";
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
+			for(String line: lines) {
+				bw.write(line);
+				bw.newLine();
+			}	
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
