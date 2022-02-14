34c08324162efb2b6860513f76dfe27157eec372
Douglasdejesus1 <douglas.dj1242@gmail.com>
2022-02-14 18:41:51 -0300

    Teste RentalCar interfaces ainda sem intefaces

--- testeInterface/src/application/Program.java
+++ testeInterface/src/application/Program.java
@@ -1,28 +1,53 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
+import model.entities.CarRental;
+import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException{
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc=new Scanner(System.in);
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy HH:ss");
+		
 		System.out.println("Enter rental data");
 		System.out.print("Car model: ");
-		String model=sc.nextLine();
-		System.out.print("Pickup (dd/MM/yyy hh:mm):");
+		String carModel=sc.nextLine();
 		
-		System.out.print("Return (dd/MM/yyy hh:mm):");
+		System.out.print("Pickup (dd/MM/yyy hh:ss):");
+		Date start = sdf.parse(sc.nextLine());
+		
+		System.out.print("Return (dd/MM/yyy hh:ss):");
+		Date finish = sdf.parse(sc.nextLine());
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		
+		
 		
 		System.out.print("Enter price per hour: ");
 		double hourPrice= sc.nextDouble();
 		System.out.print("Enter price per day: ");
 		double dayPrice= sc.nextDouble();
 		
+		RentalService rentalService=new RentalService(dayPrice, hourPrice, new BrazilTaxService());
+		
+		rentalService.processInvoice(cr);
+		
 		System.out.println("INVOICE");
+		System.out.println("Basic payment "+String.format("%.2f", cr.getInvoice().getBasicPayment()));
+		System.out.println("tax "+String.format("%.2f", cr.getInvoice().getTax()));
+		System.out.println("Total payment "+String.format("%.2f", cr.getInvoice().getTotatalPayment()));
+		
+		sc.close();
 	}
 
 }
