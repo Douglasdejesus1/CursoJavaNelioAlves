937934c77bb5497e4dedc5df45bce0a98f76c1ca
Douglasdejesus1 <douglas.dj1242@gmail.com>
2022-02-21 22:20:51 -0300

    ExercicioMapCandidatosClass216

---
+++ ExercicioMapCantidatosClass216/src/application/Program.java
@@ -0,0 +1,52 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import entities.Users;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter file full path: ");
+		String path = sc.nextLine();
+
+		File logsFile = new File(path);
+		Map<String, Integer> votes  = new TreeMap<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(logsFile))) {
+
+			String itemCsv = br.readLine();
+
+			while (itemCsv != null) {
+				String[] fields = itemCsv.split(",");
+				String candidate = fields[0];
+				int count = Integer.parseInt(fields[1]);
+				
+				if (votes.containsKey(candidate)) {
+					int votesSoFar =votes.get(candidate);
+					votes.put(candidate, count+votesSoFar);
+				}
+				else {
+					votes.put(candidate, count);
+				}
+				
+
+				itemCsv = br.readLine();
+
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
+		}
+		for (String key : votes .keySet()) {
+			System.out.println(key + ": " + votes .get(key));
+
+		}
+	}
+}
